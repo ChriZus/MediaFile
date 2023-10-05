f377d7c89b2b42d19262968b02fc7f423bd3d8b6
Chris van den Berg <chrizus@gmail.com>
2023-10-06 00:38:40 +0200

    Update AacAudioTest.php

--- tests/audios/AacAudioTest.php
+++ tests/audios/AacAudioTest.php
@@ -1,17 +1,17 @@
 <?php
-use wapmorgan\MediaFile\MediaFile;
+use BergPlaza\MediaFile\MediaFile;
 
 class AacAudioTest extends PHPUnit_Framework_TestCase
 {
 	public function testAudio1()
 	{
 		$file = MediaFile::open(FIXTURES_DIR.'/audio/audio_1.aac');
-		$this->assertInstanceOf('wapmorgan\MediaFile\MediaFile', $file);
+		$this->assertInstanceOf('BergPlaza\MediaFile\MediaFile', $file);
 		$this->assertTrue($file->isAudio());
 
         // checking audio interfac
         $audio = $file->getAudio();
-		$this->assertInstanceOf('wapmorgan\MediaFile\Adapters\AudioAdapter', $audio);
+		$this->assertInstanceOf('BergPlaza\MediaFile\Adapters\AudioAdapter', $audio);
 		$this->assertEquals(34, $audio->getLength(), '', 2);
 		$this->assertEquals(44100, $audio->getSampleRate());
 		$this->assertEquals(2, $audio->getChannels());
